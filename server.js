84873bbe9f5098899e2fd26fc506f608ea40e979
Anuj123verma <anuj17026@iiitd.ac.in>
2023-11-22 07:30:04 +0530

    basic features added

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const port = 3000
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 app.get('/', (req, res) => {
-  res.send('Hello World3!')
+  res.sendFile(path.join(__dirname, "template/index.html"))
+})
+
+app.post('/merge', upload.array('pdfs',2), function (req, res, next) {
+    
 })
 
 app.listen(port, () => {
