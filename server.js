3ced44a7ff93425f171ee9a0af811f8195e63ca4
Anuj123verma <anuj17026@iiitd.ac.in>
2023-11-23 23:53:26 +0530

    merge part added

--- server.js
+++ server.js
@@ -4,14 +4,17 @@ const path = require('path')
 const port = 3000
 const multer  = require('multer')
 const upload = multer({ dest: 'uploads/' })
+app.use('/static', express.static('public'))
+const mergedPdfs = require('./merge')
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "template/index.html"))
 })
 
-app.post('/merge', upload.array('pdfs',2), function (req, res, next) {
+app.post('/merge', upload.array('pdfs',2), async (req, res, next) => {
     console.log(req.files)
-    res.send({data : req.files})
+    await mergedPdfs.mergedPdfs(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path))
+    res.redirect("http:/localhost:3000/static/merged.pdf")
 })
 
 app.listen(port, () => {
